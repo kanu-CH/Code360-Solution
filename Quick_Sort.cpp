3b9043a631a0128d5519ff897f4ce18f32d3e913
kanu-CH <121243650+kanu-CH@users.noreply.github.com>
2024-01-09 14:33:24 +0530

    Update Quick_Sort.cpp

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -1,29 +1,40 @@
 #include <bits/stdc++.h> 
-int partition (vector<int>& arr, int l, int r)
-{
-    int x = arr[r];
-    int i = l - 1;
-    for (int j = l; j <= r- 1; j++)
-    {
-        if (arr[j] <= x)
-        {
-            i++;
-            swap(arr[i], arr[j]);
+int partition(vector<int>& arr, int s, int e) {
+    int pivot = arr[s];
+    int cnt = 0;
+    for(int i = s+1; i<=e; i++) {
+        if(arr[i] <=pivot) {
+            cnt++;
         }
     }
-    swap(arr[i + 1], arr[r]);
-    return (i + 1);
-}
- 
-void qSort(vector<int>& arr, int l, int r)
-{
-    if (l < r)
-    {
-        int q = partition(arr, l,r);
-        qSort(arr, l, q - 1);
-        qSort(arr, q + 1, r);
+
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
+
+    int i = s, j = e;
+    while(i < pivotIndex && j > pivotIndex) {
+        while(arr[i] <= pivot) 
+        {
+            i++;
+        }
+        while(arr[j] > pivot) {
+            j--;
+        }
+        if(i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
     }
+    return pivotIndex;
 }
+
+void qSort(vector<int>& arr, int s, int e) {
+    if(s >= e) 
+        return ;
+    int p = partition(arr, s, e);
+    qSort(arr, s, p-1);
+    qSort(arr, p+1, e);
+}
+
 vector<int> quickSort(vector<int> arr)
 {
     qSort(arr,0,arr.size()-1);
