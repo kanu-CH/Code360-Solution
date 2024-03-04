7c134951dc31d95c441ffd6a1bee30b9eda0c69d
kanu-CH <121243650+kanu-CH@users.noreply.github.com>
2024-03-04 21:37:00 +0530

    Update Reverse_Linked_List.cpp

--- Reverse_Linked_List.cpp
+++ Reverse_Linked_List.cpp
@@ -19,19 +19,44 @@
 
 *****************************************************************/
 
+void reverse(LinkedListNode<int>* &head, LinkedListNode<int>* curr, LinkedListNode<int>* prev) {
+    // base case
+    if(curr == NULL) {
+        head = prev;
+        return;
+    }
+
+    LinkedListNode<int>* forward = curr -> next;
+    reverse(head, forward, curr);
+    curr -> next = prev;
+}
+
+
+
 LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
 {
-    if(head ==  NULL || head -> next == NULL){
-        return head;
-    }
-    LinkedListNode<int>* prev = NULL;
+    //recursive approach
+
     LinkedListNode<int>* curr = head;
-    LinkedListNode<int>* forward = NULL;
-    while(curr!=NULL){
-        forward = curr -> next;
-        curr -> next = prev;
-        prev = curr;
-        curr = forward;
-    }
-    return prev;
+    LinkedListNode<int>* prev = NULL;
+    reverse(head, curr, prev);
+    return head;
+
+
+
+    //iterative approach
+
+    // if(head ==  NULL || head -> next == NULL){
+    //     return head;
+    // }
+    // LinkedListNode<int>* prev = NULL;
+    // LinkedListNode<int>* curr = head;
+    // LinkedListNode<int>* forward = NULL;
+    // while(curr!=NULL){
+    //     forward = curr -> next;
+    //     curr -> next = prev;
+    //     prev = curr;
+    //     curr = forward;
+    // }
+    // return prev;
 }
