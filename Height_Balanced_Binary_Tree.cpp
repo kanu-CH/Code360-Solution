85447227efc2910da5b35ec611a65756c9bbd2cf
kanu-CH <121243650+kanu-CH@users.noreply.github.com>
2024-05-06 22:30:57 +0530

    Create Height_Balanced_Binary_Tree.cpp

---
+++ Height_Balanced_Binary_Tree.cpp
@@ -0,0 +1,53 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode
+    {
+    public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val)
+        {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+pair<bool, int> isBalancedFast(TreeNode<int>* root) {
+                // base case
+        if(root == NULL)
+        {
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        
+        pair<int,int> left = isBalancedFast(root->left);
+        pair<int,int> right = isBalancedFast(root->right);
+        
+        
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+        
+        bool diff = abs (left.second - right.second ) <=1;
+        
+        pair<bool,int> ans;
+        ans.second = max(left.second, right.second) + 1;
+        
+        if(leftAns && rightAns && diff) {
+           ans.first = true;
+        }
+        else
+        {
+            ans.first = false;
+        }
+        return ans;
+    }
+bool isBalancedBT(TreeNode<int>* root){
+    return isBalancedFast(root).first;
+}
