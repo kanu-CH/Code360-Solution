50ab06ce49275a14aec88942848446041c651cab
kanu-CH <121243650+kanu-CH@users.noreply.github.com>
2024-07-18 15:40:06 +0530

    Update Topological_Sort.cpp

--- Topological_Sort.cpp
+++ Topological_Sort.cpp
@@ -1,3 +1,5 @@
+//using dfs
+
 #include <bits/stdc++.h> 
 
 void topoSort(int node, vector<bool> &visited, stack<int> &st, unordered_map<int, list<int>> &adj) {
@@ -36,6 +38,59 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
     while (!st.empty()) {
         ans.push_back(st.top());
         st.pop();
+    }
+
+    return ans;
+}
+
+
+//using bfs (Kahn's algorithm)
+
+#include <bits/stdc++.h> 
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // adjacency list
+    unordered_map<int, list<int>> adj;
+
+    for (int i=0; i<edges.size(); i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+    }
+
+    // finding indgrees of vertices
+    vector<int> indegree(v);
+    for (auto i : adj) {
+        for (auto j : i.second) {
+            indegree[j]++;
+        }
+    }
+
+    queue<int> q;
+    
+    // pushing vertices having 0 indegree
+    for (int i=0; i<indegree.size(); i++) {
+        if (indegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    vector<int> ans;
+
+    while (!q.empty()) {
+        int front = q.front();
+        q.pop();
+
+        ans.push_back(front);
+
+        for (auto neighbour : adj[front]) {
+            indegree[neighbour]--;
+
+            if (indegree[neighbour] == 0) {
+                q.push(neighbour);
+            }
+        }
     }
 
     return ans;
