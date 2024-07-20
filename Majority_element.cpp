8001a1431579c2959f7f629f6d699619b815217b
kanu-CH <121243650+kanu-CH@users.noreply.github.com>
2024-07-20 16:06:53 +0530

    Create Majority_element.cpp

---
+++ Majority_element.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+int findMajorityElement(int arr[], int n) {
+	int candidate = 0;
+    int count = 0;
+
+    // Phase 1: Finding a candidate
+    for (int i = 0; i < n; i++) {
+        if (count == 0) {
+            candidate = arr[i];
+            count = 1;
+        } else if (arr[i] == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    // Phase 2: Verification
+    count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == candidate) {
+            count++;
+        }
+    }
+
+    if (count > n / 2) {
+        return candidate;
+    } else {
+        return -1;
+    }
+}
