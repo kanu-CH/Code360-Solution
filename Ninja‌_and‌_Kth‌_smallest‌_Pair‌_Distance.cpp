2787410689e12f83de73c764dcfa6f5241c81395
Kanika Chaudhary <121243650+kanu-CH@users.noreply.github.com>
2024-08-14 16:40:01 +0530

    Create Ninja‌_and‌_Kth‌_smallest‌_Pair‌_Distance.cpp

---
+++ Ninja‌_and‌_Kth‌_smallest‌_Pair‌_Distance.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+int countPairs(vector<int>& nums, int distance) {
+    int count = 0, left = 0;
+    for (int right = 1; right < nums.size(); ++right) {
+        while (nums[right] - nums[left] > distance) {
+            ++left;
+        }
+        count += right - left;
+    }
+    return count;
+}
+int smallestDistancePair(vector<int> &nums, int k) 
+{
+    sort(nums.begin(), nums.end());
+    int low = 0, high = nums.back() - nums.front();
+        
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (countPairs(nums, mid) < k) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+        
+    return low;
+}
