66f1b688e317baa224bc764e6222c0cc87b77534
Kanika Chaudhary <121243650+kanu-CH@users.noreply.github.com>
2024-08-27 20:11:37 +0530

    Create Partition_Equal_Subset_Sum.cpp

---
+++ Partition_Equal_Subset_Sum.cpp
@@ -0,0 +1,40 @@
+bool solve(int total, vector<int> &arr, int N){
+    vector<int> curr(total/2 +1);
+    vector<int> next(total/2 +1);
+        
+    curr[0] = 1;
+    next[0] = 1;
+        
+    for(int index = N-1; index >= 0; index--){
+        for(int target = 0; target <= (total/2); target++){
+                
+            bool include = 0;
+                
+            if(target - arr[index] >=0)
+                include = next[target - arr[index]];
+                    
+            bool exclude = next[target - 0];
+                
+            curr[target] = (include or exclude);
+        }
+            
+        next = curr;
+    }        
+    return next[total/2];
+}
+bool canPartition(vector<int> &arr, int n)
+{
+	int total = 0;  
+    for(int i=0; i<n; i++){
+        total += arr[i];
+    }
+        
+    if(total & 1)
+        return false;
+        
+    int target = total / 2;
+    if(solve(total, arr, n))
+        return true;
+    else
+        return false;
+}
