4474110a3f868e88b9b808aa61926ad769ca3f3d
Kanika Chaudhary <121243650+kanu-CH@users.noreply.github.com>
2024-09-23 20:09:55 +0530

    Create Unique_Binary_Search_Trees.cpp

---
+++ Unique_Binary_Search_Trees.cpp
@@ -0,0 +1,11 @@
+long long int numTrees(int n) 
+{
+	vector<long long int> dp(n+1,0);
+    dp[0] = dp[1] = 1;
+    for(int i=2; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            dp[i] += dp[j-1] * dp[i-j];
+        }
+    }
+    return dp[n];
+}
