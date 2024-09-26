781282687852071ce74b345ff3205e4a2bc0780c
Kanika Chaudhary <121243650+kanu-CH@users.noreply.github.com>
2024-09-26 20:42:42 +0530

    Create Best_Time_to_Buy_and_Sell_Stock.cpp

---
+++ Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int small=prices[0];
+    int maxi=INT_MIN;
+    int diff;
+    for(int i=0;i<prices.size();i++){
+        if(prices[i]>small){
+            diff=prices[i]-small;
+            maxi=max(maxi,diff);
+        }
+        else{
+            small=prices[i];
+        }
+    }
+    if(maxi==INT_MIN) return 0;
+    return maxi;
+}
